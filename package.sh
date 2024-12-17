9a296d28f3a9791ef92381dec8e60fda5410e7b8
xchellx <jon.xjonx.nat@gmail.com>
2024-12-17 06:58:32 -0500

    Final package script update

--- package.sh
+++ package.sh
@@ -20,8 +20,8 @@ if [ -d "$dp0/build" ] ; then
         cp -f -R "$dp0/licenses/." "$dp0/build/package/txtrtool/licenses"
         cp -f -R "$dp0/extern/gxtexture_base/licenses/." "$dp0/build/package/txtrtool/licenses"
         VERSION="$(git describe --exact-match --tags 2> /dev/null)"
-        [ $? -ne 0 ] && [ -z "$(git status --porcelain 2> /dev/null)" ] && VERSION="$(git rev-parse --short HEAD 2> /dev/null)" || VERSION="$(git rev-parse --short HEAD 2> /dev/null)-dirty"
-        [ $? -ne 0 ] && VERSION="unknown"
+        [ -z "${VERSION}" ] && [ -z "$(git status --porcelain 2> /dev/null)" ] && VERSION="$(git rev-parse --short HEAD 2> /dev/null)" || VERSION="$(git rev-parse --short HEAD 2> /dev/null)-dirty"
+        [ -z "${VERSION}" ] && VERSION="unknown"
         7za a -ssw -t7z -mx=9 -myx=9 -ms=off -mmt=16 -mmtf=on -mtm=off -mtc=off -mta=off -m0=LZMA2:d=256m:mf=bt4:lc=4:fb=256 "$dp0/build/txtrtool_${VERSION}_msys2-clang64_win-x64.7z" "$dp0/build/package/txtrtool/"
         return 0 2>/dev/null
         exit 0
